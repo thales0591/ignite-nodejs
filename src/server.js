260fc8ba07c02af073ae438171dc4a81d1930d50
Thales <thales0591@gmail.com>
2025-02-18 21:30:59 -0300

    implement the delete user functionality

--- src/server.js
+++ src/server.js
@@ -9,11 +9,15 @@ const server = http.createServer(async (req, res) => {
 
   const route = routes.find((route) => 
   {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   }) 
 
   if (route)
   {
+    const routeParams = req.url.match(route.path)
+
+    req.params = { ...routeParams.groups }
+
     return route.handler(req, res)
   }
 
